2d92d3d57c724c6d58ecf09f5e003343a61d1b76
YOKOI KPAMISSA LEMUEL-ANGE <lyokoi@reference.ca>
2024-11-07 23:28:39 -0500

    Update suite test case

--- projects/model-mock/src/lib/model-mock.spec.ts
+++ projects/model-mock/src/lib/model-mock.spec.ts
@@ -10,23 +10,43 @@ interface SampleDataModel {
 
 describe('ModelMock', () => {
   let service: ModelMock<SampleDataModel>;
-
+   // Create an instance of ModelMock with a sample data model
+   const sampleModel: SampleDataModel = {
+    firstName: '',
+    lastName: '',
+    age: 0,
+  };
   beforeEach(() => {
     // Initialize TestBed if you need Angular testing utilities; otherwise, you can skip this.
     TestBed.configureTestingModule({});
     
-    // Create an instance of ModelMock with a sample data model
-    const sampleModel: SampleDataModel = {
-      firstName: '',
-      lastName: '',
-      age: 0,
-    };
-    service = new ModelMock<SampleDataModel>(sampleModel);
+ 
+    service = new ModelMock<SampleDataModel>(sampleModel,10);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
-  // You can add more tests below to verify functionality
+  it('should generate 10 lignes aleatoires',()=>{
+      expect(service.dataList.length).toEqual(10);
+  })
+  it('should generate string for firstName',()=>{
+      expect(typeof service.dataList[0].firstName).toBe("string");
+  })
+  it('should generate string for lastName',()=>{
+    expect(typeof service.dataList[0].lastName).toBe("string");
+  })
+  it('should generate string for age',()=>{
+    expect(typeof service.dataList[0].age).toBe("number");
+  })
+  it('should be generate zero data',()=>{
+    const emptyData = new ModelMock<SampleDataModel>(sampleModel,0);
+    expect(emptyData.dataList.length).toEqual(0);
+  })
+  it('should generate non-undefined strings for firstName and lastName', () => {
+    expect(service.dataList[0].firstName).not.toBe("");
+    expect(service.dataList[0].lastName).not.toBe('');
+  });
+
 });
